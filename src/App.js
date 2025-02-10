2932b3ba3e34fd9a7922b41220802e4dee575aa6
sharkey1508 <ryansharkey@manitex.ie>
2025-02-10 16:45:17 +0000

    update background

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-           Djotle currently under construction.
+           Djotle currently under construction
         </p>
         <img src="https://drive.google.com/drive/folders/1FuySvCv2TutFcL00UZpa26zeiS2Siwk1\Logo.jpg" className="App-logo" alt="logo" />
       </header>
