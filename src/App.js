73ea6597cb8a967326d8e67b55ddf56991770dc9
sharkey1508 <ryansharkey@manitex.ie>
2025-02-10 16:33:17 +0000

    update background

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> Djotle currently under construction.
+           Djotle currently under construction.
         </p>
         <img src="https://drive.google.com/drive/folders/1FuySvCv2TutFcL00UZpa26zeiS2Siwk1\Logo.jpg" className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
