2fd1f982ad774e44100de0b79f62d00b5a2be5a0
sharkey1508 <ryansharkey@manitex.ie>
2025-02-10 16:51:00 +0000

    update background

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <p>
            Djotle currently under construction.
         </p>
-        <img src="https://drive.google.com/file/d/165nIBU0P3DRmcghiYXNfOwTw4ehLTopP/view?usp=drive_link" className="App-logo" alt="logo" />
+        <img src="https://drive.google.com/file/d/165nIBU0P3DRmcghiYXNfOwTw4ehLTopP/Logo.jpg" className="App-logo" alt="logo" />
       </header>
     </div>
   );
